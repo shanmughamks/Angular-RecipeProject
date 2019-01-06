28965c3d9252cfc280e45cc946bdf5fcd2170c70
ShanmughamKS <ks_shan@hotmail.com>
2019-01-05 22:58:47 -0500

    Components&Databinding completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  selectedFeature: string = 'recipe';
+
+  onFeatureSelected(feature: string) {
+      this.selectedFeature = feature;
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,14 +8,19 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe ('A Test Recipe','Test', 'https://images.media-allrecipes.com/images/56589.png'),
-    new Recipe ('A Test Recipe','Test', 'https://planningwithkids.com/wp-content/2014/05/homemade-chicken-casserole-for-baby.jpg')
+    new Recipe ('A Test Recipe','Recipe description...', 'https://images.media-allrecipes.com/images/56589.png'),
+    new Recipe ('Another Test Recipe','Recipe description...', 'https://planningwithkids.com/wp-content/2014/05/homemade-chicken-casserole-for-baby.jpg')
   ];
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {    
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
